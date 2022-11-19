f8ccf07c98595d6617b83b3439bee984eabf6def
sahatsawat-kew <60878851+sahatsawat-kew@users.noreply.github.com>
2022-11-20 03:16:50 +0700

    add Authen new UI
    add css to form login(connect)

--- Applications/frontend/src/components/Authen.js
+++ Applications/frontend/src/components/Authen.js
@@ -1,30 +1,92 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2'
 
 const Authen = () => {
   const [usr, setUsr] = useState('');
   const [pwd, setPwd] = useState('');
   const [port, setPort] = useState('');
   const [status, setStatus] = useState('');
-  const login = () => {
+  const login = async () => {
     const data = { username: usr, password: pwd };
     sessionStorage.clear();
     axios.post(`http://localhost:${port}/login`, data).then((response) => {
-      try {
-        if (response.data.token) {
+
+      if (response.data.token.length !== 0) {
+           verifly_token(response.data.token);
           sessionStorage.setItem('token', response.data.token);
           sessionStorage.setItem('user', usr);
           setStatus(`Login as ${sessionStorage.getItem('user')}`)
-        }
-      } catch (error) {
-        console.log(error);
+          
+      }
+      
+    }).catch(err => {
+      if(err.name === 'AxiosError'){
+        // console.log(err.response.status);
+        
+        Swal.fire({
+            icon: 'error',
+            title: "Connect Fail",
+            text:err.message,
+            confirmButtonColor: "#FF0000",
+            confirmButtonText: "Close",
+        })
+        
       }
     });
   };
+
+
+  const verifly_token = (token) => {
+    
+    // console.log("param verifly = ",token);
+    const config = {
+      headers: { Authorization: `Bearer ${token}` }
+    };
+    
+    const bodyParameters = {
+      "jsonrpc":"2.0",
+      "method": "priv_createPrivacyGroup", 
+      "params": 
+      [
+          {
+          "addresses":[
+              "pUlpb1ZHkNfJXQbVzGkaGeAtGE/qTsmKWeR6/ilZRC8=",
+              "9sTR0aTy3x8tTVZPIAuJCAdLf0u0Fq1kYp2HucnNUmQ=",
+              "16YSE4L7vFYlcErmE74IxSxp5NrG+tkN7VL7Nnrl0DM="
+          ],
+          "name":"Group A",
+          "description":"Description Group A"
+          }
+      ],
+      "id":1
+    };
+    
+      axios.post('http://localhost:8545', bodyParameters,config).then((response) => {
+      setStatus(response.request.status);
+      console.log(response.request.status);
+    // console.log('Login Status = ',status);
+      Swal.fire({
+        icon: 'success',
+        title: "Connect Success!!",
+        text: response.data.message,
+        confirmButtonColor: "#00FF00",
+        confirmButtonText: "OK",
+      })
+      // return response;
+      
+    });
+   
+    
+  };
   return (
-    <div className='authen'>
-      <h3>Authentication</h3>
-      <h4>{status}</h4>
+    <div className='authen '>
+       <div className='center '>
+          <h2 className='login_title center '>Login</h2>
+      </div>
+      {/* <h3>Authentication</h3> */}
+      {/* <h4>{status}</h4> */}
+      <div class="center form">
       <label>PORT</label>
       <br />
       <input
@@ -41,18 +103,23 @@ const Authen = () => {
         onChange={(e) => {
           setUsr(e.target.value);
         }}
+        placeholder="Enter Username" name="uname" required
       />
       <br />
       <label>Password</label>
       <br />
       <input
-        type='text'
+        type='password'
         onChange={(e) => {
           setPwd(e.target.value);
         }}
+        placeholder="Enter Password" name="psw" required
       />
       <br />
-      <button onClick={login}>Login</button>
+      <button className=" button_connect center" onClick={login}>CONNECT</button>
+      </div>
+      
+      
     </div>
   );
 };
