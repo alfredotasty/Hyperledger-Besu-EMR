75b1a373360091d6ab7f3e0f60a6a76508071518
Nammon <supamongkon.r@gmail.com>
2022-11-20 22:14:54 +0700

    add timeline

--- Applications/frontend/src/components/History.js
+++ Applications/frontend/src/components/History.js
@@ -5,21 +5,32 @@ const History = () => {
     return(
         <div>
             <h2>Admid History</h2>
-
-        <div className='fram'>
-            <div >
+        <div className='timeline'>
+        <div>
+            <h3 className="first">First</h3>
+        </div>
+            <div className='outer'>
                 {user.map((user, i) => (
-                    <div className='square' key={i}>
-                        <p>From: {user}</p>
-                        <p>Detail: {user.address}</p>
-                        <p>Date : {user.node}</p>       
-                        <hr></hr>          
-                    </div>
-        
+                    <div className='card' key={i}>
+                        <div className="title">
+                            <div className= 'info'>
+                                <h3 >History</h3>
+                                <p>From: {user}</p>
+                                <p>Detail: {user.address}</p>
+                                <p>Date : {user.node}</p> 
+                             
+                            </div>                           
+                        </div>
+                    </div>    
                 ))}
             </div>
+            <div>
+                <h3 className="last">Last</h3>
+                
+            </div>
+            </div>
         </div>
-        </div>
+
     )
 }
 export default History;
