e7b8fa9daa27b3870d4f7e8f1000a146497f4928
maximus3642 <71571531+maximus3642@users.noreply.github.com>
2022-11-20 23:25:30 +0700

    Create Users.js

---
+++ Applications/backend/Users.js
@@ -0,0 +1,28 @@
+module.exports = {
+  users: {
+    patient1_1: {
+      url: 'http://localhost:8545',
+      tesseraPublicKey: 'PFf59KI9cmuc4So9vsCVBDnRZJeRNQau1vuoftI3ezU=',
+    },
+    patient1_2: {
+      url: 'http://localhost:8545',
+      tesseraPublicKey: 'k+TisZyEK+LdHwku8Us/1D4Jb9Ra0WpZBK8uJK2OG3c=',
+    },
+    patient2_1: {
+      url: 'http://localhost:8546',
+      tesseraPublicKey: 'I6kZj+Ww0fyzVqWMQbvA6JsNO9ce4EPW0bzWw4n3Plg=',
+    },
+    patient2_2: {
+      url: 'http://localhost:8546',
+      tesseraPublicKey: 'HPJcShaJt11j9F0WEwdADrFrEI2wM8X4smxd+9SpDQA=',
+    },
+    patientA: {
+      url: 'http://localhost:8545',
+      tesseraPublicKey: 'Cz+49epNhfIayoe4vo884qi/rqDYCVoFo9BiKBJcYXk=',
+    },
+    patientB: {
+      url: 'http://localhost:8546',
+      tesseraPublicKey: 'qyf4ksC+eEn+pZVNROwm+N1eEsmOR+prg13lJh0Y+CM=',
+    },
+  },
+};
